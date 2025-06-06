029334a4aafc5abe386d97ce79865261cc566259
aobungu <aobungu93@gmail.com>
2025-06-06 10:50:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app7")
+st.title("🎈 My new app7a")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
